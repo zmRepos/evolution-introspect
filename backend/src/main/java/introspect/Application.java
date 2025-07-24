1ccb35449378605a8d6843e95b6b893ba35dd163
zzz <2228241928@qq.com>
2025-07-25 00:57:39 +0800

    init: 项目初始化

---
+++ backend/src/main/java/introspect/Application.java
@@ -0,0 +1,13 @@
+package introspect;
+
+/**
+ * @author zzz
+ * @date 2025/7/24-19:05
+ * @desc 启动类
+ */
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.evolution.introspect")
+public class Application {
+}
---
+++ backend/src/main/java/introspect/controller/CodeGeneratorController.java
@@ -0,0 +1,16 @@
+package introspect.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zzz
+ * @date 2025/7/24-19:07
+ * @desc 代码生成器控制器
+ */
+@RestController
+@RequestMapping("/code-generator")
+@Tag
+public class CodeGeneratorController {
+
+}
